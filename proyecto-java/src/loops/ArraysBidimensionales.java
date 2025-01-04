cf7aa944ee5a203890dad18f8f9ad26b65ba3efd
George Maxi <31070682+Brianmax@users.noreply.github.com>
2025-01-03 19:41:20 -0500

    arrays bidimensionales

---
+++ proyecto-java/src/loops/ArraysBidimensionales.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class ArraysBidimensionales {
+    public static void main(String[] args) {
+        int[][] arrayBidimensional = new int[3][3];
+
+        int[][] arrayBidimensional2 = {{0,2,3,8}, {1,5,2,9},{4,2,1,0}};
+
+        for (int i = 0; i < arrayBidimensional2.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(arrayBidimensional2[i][j]);
+            }
+        }
+    }
+}
